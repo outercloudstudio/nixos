133919e5b4ec1adfcd714f730b1b934b05f109d3
Outer Cloud <outercloudstudio@gmail.com>
2025-08-18 21:52:01 -0500

    Update launch.sh

--- liamh/fzf-launcher/scripts/launch.sh
+++ liamh/fzf-launcher/scripts/launch.sh
@@ -14,7 +14,7 @@ selected=$(compgen -c | fzf)
 
 if [ -n "$selected" ]; then
     if is_gui_app "$selected"; then
-        "$selected"
+        "$selected & disown"
     else
         setsid kitty -e "$selected" & sleep 1
     fi
