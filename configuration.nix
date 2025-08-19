67e67be5df84aa0306c9a6d9de0f5c70157e723f
Outer Cloud <outercloudstudio@gmail.com>
2025-08-19 04:14:43 -0500

    Add hyprpolkit

--- configuration.nix
+++ configuration.nix
@@ -147,6 +147,7 @@
   xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
 
   security.polkit.enable = true;
+  services.hyprpolkitagent.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
