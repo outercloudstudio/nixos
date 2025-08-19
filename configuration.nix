bdaf7747ce1a5ebd85c7a8b8c1863ccf48fa1616
Outer Cloud <outercloudstudio@gmail.com>
2025-08-19 00:23:32 -0500

    Add iwd

--- configuration.nix
+++ configuration.nix
@@ -25,6 +25,8 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
+  networking.wireless.iwd.enable = true;
+  networking.networkmanager.wifi.backend = "iwd";
 
   # Set your time zone.
   time.timeZone = "America/Los_Angeles";
