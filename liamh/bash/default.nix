c7776570b2892038afb3cc0096075cbcd7222301
Outer Cloud <outercloudstudio@gmail.com>
2025-08-22 23:41:34 -0500

    Move bash config

---
+++ liamh/bash/default.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+
+{
+    programs.bash = {
+        enable = true;
+        enableCompletion = true;
+        bashrcExtra = ''
+        export EDITOR='nano'
+        export PATH="$HOME/bin:$PATH"
+
+        alias rebuild="sudo bash ~/bin/rebuild.sh"
+        alias cleanup="sudo nix-collect-garbage -d && sudo nixos-rebuild switch"
+        alias gemma="(ollama serve &> /dev/null) & (sleep 1 && ollama pull gemma3:4b-it-qat &> /dev/null && ollama run gemma3:4b-it-qat)"
+        alias record="asak rec"
+        alias play="asak play"
+        '';
+    };
+}
--- liamh/home.nix
+++ liamh/home.nix
@@ -13,24 +13,10 @@
     ./scripts
     ./hyprpaper
     ./git
+    ./bash
   ];
 
   home.packages = with pkgs; [];
-
-  programs.bash = {
-    enable = true;
-    enableCompletion = true;
-    bashrcExtra = ''
-      export EDITOR='nano'
-      export PATH="$HOME/bin:$PATH"
-
-      alias rebuild="sudo bash ~/bin/rebuild.sh"
-      alias cleanup="sudo nix-collect-garbage -d && sudo nixos-rebuild switch"
-      alias gemma="(ollama serve &> /dev/null) & (sleep 1 && ollama pull gemma3:4b-it-qat &> /dev/null && ollama run gemma3:4b-it-qat)"
-      alias record="asak rec"
-      alias play="asak play"
-    '';
-  };
 
   programs.helix = {
     enable = true;
