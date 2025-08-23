b662246db103fba3a119d028adee0f9bee3fa639
outercloudstudio <outercloudstudio@gmail.com>
2025-08-23 11:02:27 -0700

    Git credentials

--- liamh/git/default.nix
+++ liamh/git/default.nix
@@ -5,5 +5,9 @@
         enable = true;
         userName = "outercloudstudio";
         userEmail = "outercloudstudio@gmail.com";
+
+        extraConfig.credential.helper = "manager";
+        extraConfig.credential."http://git.htlec.org".username = "outercloudstudio";
+        extraConfig.crednetial.credentialStore = "cache";
     };
 }
