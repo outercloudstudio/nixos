1b7485ddf068eb3b0902e73b1df93ced877f1e82
outercloudstudio <outercloudstudio@gmail.com>
2025-08-23 11:24:12 -0700

    Change credential store

--- liamh/git/default.nix
+++ liamh/git/default.nix
@@ -8,7 +8,7 @@
 
         extraConfig.credential.helper = "manager";
         extraConfig.credential."http://github.com".username = "outercloudstudio";
-        extraConfig.credential.credentialStore = "secretservice";
+        extraConfig.credential.credentialStore = "cache";
     };
     
     home.packages = with pkgs; [ git-credential-manager ];
