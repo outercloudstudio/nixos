1e15f2748bf79dfafe1d7bff0e4260ebe4030875
Outer Cloud <outercloudstudio@gmail.com>
2025-08-23 22:32:07 -0500

    Gnome keyring

--- configuration.nix
+++ configuration.nix
@@ -142,6 +142,7 @@
   # xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
 
   security.polkit.enable = true;
+  services.gnome-keyring.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
