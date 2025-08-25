dd7edda184a3113bc3758cd4be35bc4577c3ae3d
Outer Cloud <outercloudstudio@gmail.com>
2025-08-24 21:19:39 -0500

    Update default.nix

--- liamh/git/default.nix
+++ liamh/git/default.nix
@@ -10,8 +10,6 @@
         extraConfig.credential."https://github.com".username = "outercloud";
         extraConfig.credential.credentialStore = "cache";
     };
-
-    programs.seahorse.enable = true;
     
     home.packages = with pkgs; [ git git-credential-manager ];
 }
