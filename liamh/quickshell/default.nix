991d6dcc3d7824d7d67c2261994c79d5f9e6edb6
outercloudstudio <outercloudstudio@gmail.com>
2025-09-30 10:23:17 -0400

    Write config to config folder

--- liamh/quickshell/default.nix
+++ liamh/quickshell/default.nix
@@ -1,6 +1,11 @@
 { config, pkgs-unstable, ... }:
 
 {
+    home.file.".config/quickshell" = {
+        source = ./config;
+        recursive = true;
+    };
+
     home.packages = with pkgs-unstable; [
         quickshell
     ];
